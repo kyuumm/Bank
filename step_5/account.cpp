ab5f2d738891a250b2fbfe4670eba38c0f1952ed
kyuumm <kyuuyxx@outlook.com>
2025-06-02 17:45:55 +0800

    step_5动态添加新账户，AccountRecord类，修改Date类

---
+++ step_5/account.cpp
@@ -0,0 +1,130 @@
+//
+// Created by kyuu on 2025/6/2.
+//
+
+
+#include "account.h"
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+double Account::total = 0;
+
+RecordMap Account::recordMap;
+
+Account::Account(const Date &date, const string &id):id(id),balance(0){
+    date.show();
+    cout << "\t#" << id << " created" << endl;
+}
+
+void AccountRecord::show()const{
+    date.show();
+    cout<<"\t#"<<account->getId()<<"\t"<<amount<<"\t"<<balance<<"\t"<<desc<<endl;
+}
+
+void Account::record(const Date &date, double amount, const string &desc) {
+//    accumulation = accumulate(date);
+//    lastDate = date;
+    amount = floor(amount * 100 + 0.5) / 100;	//保留小数点后两位
+    balance += amount;
+
+    AccountRecord record(date,this,amount,balance,desc);
+
+
+    recordMap.insert(make_pair(date,record));
+
+    record.show();
+}
+
+
+void Account::error(const string &msg) const {
+    cout << "Error(#" << id << "): " << msg << endl;
+}
+
+void Account::show() const {
+    cout << id << "\tBalance: " << balance;
+}
+
+void Account::query(const Date&begin,const Date&end){
+    if (begin < end){
+        RecordMap ::iterator  iter1=recordMap.lower_bound(begin);
+        RecordMap ::iterator  iter2=recordMap.lower_bound(begin);
+        for ( RecordMap::iterator iter=iter1;iter!=iter2   ; ++iter) {
+            iter->second.show();
+        }
+    }
+}
+
+//SavingsAccount类相关成员函数的实现******************************************
+SavingsAccount::SavingsAccount(const Date &date, const string &id, double rate)
+        : Account(date,id), rate(rate), acc(date,0) {
+
+}
+
+void SavingsAccount::deposit(const Date &date, double amount, const string &desc) {
+    record(date, amount, desc);
+    acc.change(date,getBalance());
+}
+
+void SavingsAccount::withdraw(const Date &date, double amount, const string &desc) {
+    if (amount > getBalance())
+        error("not enough money");
+    else{
+        record(date, -amount, desc);
+        acc.change(date,getBalance());
+    }
+
+}
+
+void SavingsAccount::settle(const Date &date) {
+    if(date.getMonth()==1&&date.getDay()==1){
+        double interest = acc.getSum(date,getBalance()) * rate	//计算年息
+                          / (date-Date(date.getYear() - 1, 1, 1));
+        if (interest != 0)
+            record(date, interest, "interest");
+        acc.reset(date,getBalance());
+    }
+
+}
+
+
+//CreditAccount实现**********************************************
+
+CreditAccount::CreditAccount(const Date &date, const string &id, double credit, double rate, double fee)
+        :Account(date,id), credit(credit), rate(rate), fee(fee), acc(date,0) {
+}
+
+void CreditAccount::deposit(const Date &date, double amount, const string &desc) {
+    record(date, amount, desc);
+    acc.change(date,getDebt());
+}
+
+void CreditAccount::withdraw(const Date &date, double amount, const string &desc) {
+    if (amount > getBalance()+credit)
+        error("not enough credit");
+    else{
+        record(date, -amount, desc);
+        acc.change(date,getDebt());
+    }
+}
+
+void CreditAccount::settle(const Date &date) {
+    double interest = acc.getSum(date,getDebt()) * rate;//计算年息
+    if (interest != 0)
+        record(date, interest, "interest");
+    if(date.getMonth()==1){
+        record(date,-fee,"annual fee");
+    }
+    acc.reset(date,getDebt());
+}
+
+void CreditAccount::show()const{
+    Account::show();
+    cout<<"\tAvailable credit:"<<getAvailableCredit();
+}
+
+AccountRecord::AccountRecord(const Date&date,const Account *account,double amount,double balance,const string &desc):date(date),account(account),amount(amount),balance(balance),desc(desc){}
+
+
+
+
