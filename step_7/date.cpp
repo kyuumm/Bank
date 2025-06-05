59306659df70e9a47e81cc02c05ced8c949fb352
kyuumm <kyuuyxx@outlook.com>
2025-06-05 17:30:49 +0800

    step_7第一部分：异常处理机制

---
+++ step_7/date.cpp
@@ -0,0 +1,44 @@
+//
+// Created by kyuu on 2025/6/2.
+//
+#include"date.h"
+#include<iostream>
+#include<stdexcept>
+
+using namespace std;
+
+namespace{//namespace 下面的定义只在当前文件中有效
+    const int DAYS_BEFORE_MONTH[]={0,31,59,90,120,151,181,212,243,273,304,334,365};
+}
+Date::Date(int year, int month, int day) {
+
+    if (month<1||month>12||day<1||day>getMaxDay()){throw runtime_error("Invalid date");}
+
+    this->year=year;
+    this->month=month;
+    this->day=day;
+
+    int years=year-1;
+    totalDays=years*365+ years/4-years/100+years/400 + DAYS_BEFORE_MONTH[month-1]+day;
+    if(isLeapYear()&&month>2) totalDays++;
+}
+
+int Date::getMaxDay() const {
+    if (isLeapYear()&&month==2) return 29;
+    else return DAYS_BEFORE_MONTH[month]-DAYS_BEFORE_MONTH[month-1];
+}
+
+void Date::show() const {
+    cout<<getYear()<<"-"<<getMonth()<<"-"<<getDay();
+}
+
+Date Date::read(){
+    int year , month , day;
+    char c1,c2;
+
+    cin>>year>>c1>>month>>c2>>day;
+
+    if(!cin) throw runtime_error("Failed to read date");
+
+    return Date(year,month,day);
+}
---
+++ step_7/date.h
@@ -0,0 +1,60 @@
+//
+// Created by kyuu on 2025/6/2.
+//
+
+#ifndef BANK_DATE_H
+#define BANK_DATE_H
+
+class Date{
+private:
+    int year;
+    int month ;
+    int day;
+
+    int totalDays;
+public:
+    Date(int year=1,int month=1 ,int day=1);
+
+    int getYear()const{return year;}
+    int getMonth()const{return month;}
+    int getDay()const{return day;}
+    int getMaxDay()const;
+
+    bool isLeapYear()const{
+        return year%4==0 && year%100!=0 ||year%400==0;
+    }
+
+    void show()const;
+
+//    int distance(const Date&date)const{
+//        return totalDays-date.totalDays;
+//    }
+
+// operator:
+// 运算符重载（operator overloading），
+// 是 C++ 的一个非常重要的特性，
+// 用来让你自己定义的类像内置类型一样使用常见运算符
+// （比如 -、<、==、+ 等等）。
+
+
+    //日期相差多少天
+    int operator - (const Date&date)const{
+        return totalDays-date.totalDays;
+    }
+    //日期先后顺序
+    bool operator < (const Date&date)const{
+        return totalDays < date.totalDays;
+    }
+    bool operator <= (const Date&date)const{
+        return totalDays < date.totalDays;
+    }
+
+    //static: 这个函数不需要任何对象就可以调用
+    //返回值是Date： 调用这个函数会得到一个新的Date对象
+    //使用方法：  Date d = Date::read();   不是d.read不要搞混了！
+    static Date read();
+
+
+};
+
+#endif //BANK_DATE_H
